3e296935be86d621071ab472b159db3169fff133
Wisnu Widianto <wisnuprsj@gmail.com>
2022-11-29 19:33:38 +0700

    finished section 3

--- Study/DevOps/Docker/Docker and Kubernetes The Practical Guide/data-volumes-01-starting-setup/server.js
+++ Study/DevOps/Docker/Docker and Kubernetes The Practical Guide/data-volumes-01-starting-setup/server.js
@@ -1,6 +1,7 @@
 const fs = require("fs").promises;
 const exists = require("fs").exists;
 const path = require("path");
+const PORT = process.env.PORT;
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -31,6 +32,8 @@ app.post("/create", async (req, res) => {
   const tempFilePath = path.join(__dirname, "temp", adjTitle + ".txt");
   const finalFilePath = path.join(__dirname, "feedback", adjTitle + ".txt");
 
+  console.log("TEST");
+
   await fs.writeFile(tempFilePath, content);
   exists(finalFilePath, async (exists) => {
     if (exists) {
@@ -43,4 +46,6 @@ app.post("/create", async (req, res) => {
   });
 });
 
-app.listen(80);
+app.listen(PORT, () => {
+  console.log("Server is starting on port 80");
+});
